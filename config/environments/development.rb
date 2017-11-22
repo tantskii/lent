f5de6100aa95b9ee6cb70b3f6f6a68b1084c311d
drapes <tihon1997@gmail.com>
2017-11-22 21:00:23 +0300

    develop

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     user_name: 'tihonsergienko',
-    password: 'lfqdjqnbdgjxnegflkftothfp1',
+    password: '',
     authentication: 'plain',
     enable_starttls_auto: true
   }
