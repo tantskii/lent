4710bde4c0d1543edcee866299c821a9dabf7cf0
drapes <tihon1997@gmail.com>
2017-11-28 22:21:35 +0300

    fix miswrite

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "mylent.ru", user: "deploy", roles: %w{app db web resque_worker :resque_scheduler}
+server "mylent.ru", user: "deploy", roles: %w{app db web resque_worker resque_scheduler}
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
