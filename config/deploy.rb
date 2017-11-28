64c2a40d8b10077cbf47f9255f4f1eed6f3a1f0b
drapes <tihon1997@gmail.com>
2017-11-28 22:01:14 +0300

    resque scheduler is ready to setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,7 @@ append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", 'vendor/bundle', "public/system", "public/uploads"
 
 after 'deploy:restart', 'resque:restart'
+after 'deploy:restart', 'resque:scheduler:restart'
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
